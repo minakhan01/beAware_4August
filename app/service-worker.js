e06b1f4cef09869fe923fb4fd4ebc743c3a604bd
minakhan01 <minakhan01@gmail.com>
2016-08-15 13:13:36 -0400

    login with firebase and google working, need to fix toolbars

--- app/service-worker.js
+++ app/service-worker.js
@@ -8,5 +8,5 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 console.info('Service worker disabled for development, will be generated at build time.');
-importScripts('bower_components/platinum-sw/service-worker.js');
+// importScripts('bower_components/platinum-sw/service-worker.js');
 
